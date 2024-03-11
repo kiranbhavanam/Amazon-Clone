82fc680ac489f41f71b18bceaa7b91639989b5c6
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-03-11 19:34:27 +0530

    Generate html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-
-export const cart=[];
-export function addToCart(productId,quantity){
-    let matchingItem;
-    cart.forEach((item) => {
-      if (productId === item.productId) matchingItem = item;
-    });
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({ productId, quantity });
-    }
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 10,
+  },
+];
+export function addToCart(productId, quantity) {
+  let matchingItem;
+  cart.forEach((item) => {
+    if (productId === item.productId) matchingItem = item;
+  });
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({ productId, quantity });
   }
+}

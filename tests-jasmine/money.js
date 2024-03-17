4c258d3b196864f33394c193201daa6157d5c767
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-03-17 12:16:48 +0530

    Added all necessary tests for money

--- tests-jasmine/money.js
+++ tests-jasmine/money.js
@@ -4,4 +4,10 @@ describe("testSuite for format Currency: ", () => {
   it("Converts cents to dollars", () => {
     expect(formatCurrency(2095)).toEqual("20.95");
   });
+  it("check with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+  it("Rounds up to nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
 });
